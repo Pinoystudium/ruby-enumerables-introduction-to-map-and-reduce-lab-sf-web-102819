d01db2e0f46f193225fb57369e7ca5354ab3a0a8
Michael Espina <michael_espina@yahoo.com>
2019-10-27 03:02:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(source_array)
 new_array =[]
 index = 0
 while index < source_array.length do
-  new_array << (source_arrayarray[index] * -1)
+  new_array << (source_array[index] * -1)
   index +=1
 end
 return new_array
